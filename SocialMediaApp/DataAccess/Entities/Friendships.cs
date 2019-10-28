0240279aa25413cb8df8adac33b7c1c41fcd7cbd
yetinight <gang@ucsd.edu>
2019-10-28 15:27:43 -0500

    Added friendship entity

---
+++ SocialMediaApp/DataAccess/Entities/Friendships.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class Friendships
+    {
+        public int User1ID { get; set; }
+        public int User2ID { get; set; }
+        public DateTime DateRequestSent { get; set; }
+        public DateTime DateRequestConfirmed { get; set; } 
+        public virtual Users User1 { get; set; }
+        public virtual Users User2 { get; set; }
+
+    }
+}
