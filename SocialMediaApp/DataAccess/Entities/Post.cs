ffbf24cc8fb4ca57d667b6d5bdd02fb5a0c46395
PhatNguyen203 <Lee0ne203@gmail.com>
2019-10-28 15:58:40 -0500

    Added Entities

---
+++ SocialMediaApp/DataAccess/Entities/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+	public class Post
+	{
+		public int PostId { get; set; }
+		public int UserId { get; set; }
+		public DateTime PostedDate { get; set; }
+		public string Content { get; set; }
+
+	}
+}
