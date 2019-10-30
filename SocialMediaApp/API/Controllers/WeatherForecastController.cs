e717d206b317bde44581fbf522d9b4551a733206
yetinight <gang@ucsd.edu>
2019-10-30 13:52:59 -0500

    removed angular and added api project

--- SocialMediaApp/API/Controllers/WeatherForecastController.cs
+++ SocialMediaApp/API/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace WebApp.Controllers
+namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
