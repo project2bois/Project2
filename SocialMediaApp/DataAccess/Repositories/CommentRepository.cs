b1d1704ec75f1cc87abe5e3de8d974c12f83d225
PhatNguyen203 <Lee0ne203@gmail.com>
2019-10-29 22:40:46 -0500

    Added CommentRepositories

---
+++ SocialMediaApp/DataAccess/Repositories/CommentRepository.cs
@@ -0,0 +1,55 @@
+﻿using Library.Interfaces;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace DataAccess.Repositories
+{
+	public class CommentRepository : ICommentRepository, IDisposable
+	{
+		private readonly NotTwitterDbContext _context;
+
+		public CommentRepository(NotTwitterDbContext db)
+		{
+			_context = db;
+		}
+
+		public void CreateComment(string commentText, int userId, int postId)
+		{
+			var comment = new Comment
+			{
+				User = new User { UserID = userId },
+				Post = new Post { PostID = postId},
+				Content = commentText
+			};
+
+			_context.Add(comment);
+			_context.SaveChanges();
+		}
+
+		public void DeleteCommentsByPostId(int postId)
+		{
+			var comments = _context.Comments.Where(p => p.PostId == postId);
+			foreach (var comment in comments)
+			{
+				_context.Remove(comment);
+			}
+
+			_context.SaveChanges();
+		}
+
+		public IEnumerable<Comment> CommentsByPostId(int postId)
+		{
+			return _context.Comments.Where(p => p.PostId == postId).OrderByDescending(d => d.TimeSent).Select(Mapper.MapComments);
+		}
+
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SocialMediaApp/Library/Interfaces/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Models;
+
+
+namespace Library.Interfaces
+{
+	public interface ICommentRepository : IDisposable
+	{
+		public void CreateComment(string commentText, int userId, int postId);
+
+		public void DeleteCommentsByPostId(int postId);
+
+		public IEnumerable<Models.Comment> CommentsByPostId(int postId);
+	}
+}
