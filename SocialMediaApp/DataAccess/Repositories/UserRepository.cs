c2ae80ab9c075b86907a76bc7e733dc53c6a6846
yetinight <gang@ucsd.edu>
2019-10-29 21:28:52 -0500

    Filled out most functionality for user repository

--- SocialMediaApp/DataAccess/Repositories/UserRepository.cs
+++ SocialMediaApp/DataAccess/Repositories/UserRepository.cs
@@ -2,6 +2,7 @@
 using Library.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Repositories
@@ -20,16 +21,69 @@ namespace DataAccess.Repositories
         }
 
         /// <summary>
-        /// 
+        /// Given an ID, returns matching user from DB
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">User ID to be searched for</param>
+        /// <returns>User matching the given ID</returns>
         public User GetUserByID(int id)
         {
-            //TODO: should be done after we establish a Mapper class
+            return Mapper.MapUsers( _context.Users.Find(id) );
+        }
 
-            // return Mapper.MapUser( _context.Users.Find(id) );
-            throw new NotImplementedException();
+        /// <summary>
+        /// Returns list of users with name matching given string
+        /// </summary>
+        /// <param name="name"></param>
+        /// <remarks>Checks combination of user's first and last name</remarks>
+        /// <returns></returns>
+        public IEnumerable<User> GetUsersByName(string name)
+        {
+            return _context.Users
+                .Where(u => (u.FirstName + u.LastName)
+                .Contains(name))
+                .Select(Mapper.MapUsers);
+        }
+
+        /// <summary>
+        /// Given a business model user, add user to database
+        /// </summary>
+        /// <param name="newUser">User to add to db</param>
+        public void AddUser(User newUser)
+        {
+            var newEntity = Mapper.MapUsers(newUser);
+            newUser.UserID = 0;
+            _context.Users.Add(newEntity);
+        }
+
+        /// <summary>
+        /// Update the database with new user information
+        /// </summary>
+        /// <param name="user"></param>
+        public void UpdateUser(User user)
+        {
+            var oldEntity = _context.Users.Find(user.UserID);
+            var updatedEntity = Mapper.MapUsers(user);
+
+            _context.Entry(oldEntity).CurrentValues.SetValues(updatedEntity);
+        }
+
+        /// <summary>
+        /// Deletes user from database
+        /// </summary>
+        /// <param name="id">ID of the user to be deleted</param>
+        public void DeleteUserByID(int id)
+        {
+            var entityToBeRemoved = _context.Users.Find(id);
+            _context.Remove(entityToBeRemoved);
+        }
+
+        /// <summary>
+        /// Saves changes to database
+        /// </summary>
+        public void Save()
+        {
+            // TODO: Ideally put a log message here to notify when saving
+            _context.SaveChanges();
         }
 
         #region IDisposable Support
@@ -66,6 +120,8 @@ namespace DataAccess.Repositories
             // TODO: uncomment the following line if the finalizer is overridden above.
             // GC.SuppressFinalize(this);
         }
+
+
         #endregion
 
     }
--- SocialMediaApp/Library/Interfaces/IUserRepository.cs
+++ SocialMediaApp/Library/Interfaces/IUserRepository.cs
@@ -11,7 +11,38 @@ namespace Library.Interfaces
         /// Given an ID, returns matching user from DB
         /// </summary>
         /// <param name="id">User ID to be searched for</param>
-        /// <returns></returns>
+        /// <returns>User matching the given ID</returns>
         public User GetUserByID(int id);
+
+        /// <summary>
+        /// Returns list of users with name matching given string
+        /// </summary>
+        /// <param name="name"></param>
+        /// <remarks>Checks combination of user's first and last name</remarks>
+        /// <returns></returns>
+        public IEnumerable<User> GetUsersByName(string name);
+
+        /// <summary>
+        /// Given a business model user, add user to database
+        /// </summary>
+        /// <param name="newUser">User to add to db</param>
+        public void AddUser(User newUser);
+
+        /// <summary>
+        /// Update the database with new user information
+        /// </summary>
+        /// <param name="user"></param>
+        public void UpdateUser(User user);
+
+        /// <summary>
+        /// Deletes user from database
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteUserByID(int id);
+
+        /// <summary>
+        /// Saves changes to database
+        /// </summary>
+        public void Save();
     }
 }
